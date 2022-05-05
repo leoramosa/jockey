79bbb5571e0e12182ba4714b20119652ffd8058a
Leonardo Ramos <leo.ramos.ari@gmail.com>
2022-05-05 14:46:20 -0500

    final

---
+++ recursos/js/logo.js
@@ -0,0 +1,27 @@
+let introt = document.querySelector('.introt');
+let logot = document.querySelector('.logot');
+let logoSpan = document.querySelectorAll('.logo-parts');
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    setTimeout(()=> {
+        logoSpan.forEach((span, index) => {
+          setTimeout(() => {
+              span.classList.add('active');
+          }, (index + 1) * 100); 
+        });
+
+        setTimeout(() => {
+          logoSpan.forEach((span, index)=> {
+              setTimeout(() => {
+                span.classList.remove('active');
+                span.classList.add('fade');
+              }, (span +1) * 50);
+          });
+        }, 1500);
+
+        setTimeout(()=>{
+          introt.style.top = '-100vh';
+        }, 1600);
+    });
+});
